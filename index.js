7d6f40338022eeded4f69a136ad572699c3c0ad2
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-08-19 01:35:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(array) {
+    let val = array.find(obj => obj.result === "W")
+    if (val) {
+        return val.year
+    } else {
+        return undefined
+    }
+}
